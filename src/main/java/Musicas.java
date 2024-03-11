b83c06d86aa375b820baafc8275981d88717688e
repty <repty@LAPTOP-023TN83H>
2024-03-11 17:46:51 -0600

    Merge origin/master
    # Conflicts:
    #	src/main/java/com/mycompany/clasehoy/COMENTARIO.java
    #	src/main/java/com/mycompany/clasehoy/VISUALIZACION.java

---
+++ src/main/java/Musicas.java
@@ -0,0 +1,31 @@
+public class Musicas {
+    String title; 
+    String album;
+    String date;
+    int time;
+    
+    //metodos
+    
+    /*public void Musicas(String _title, String _album, String _date, int _time){
+        title=_title;
+        album=_album;
+        date=_date;
+        time=_time;
+        
+    }*/
+    
+    //segunda forma
+    public  Musicas(String title, String album, String date, int time){
+        this.title=title;
+        this.album=album;
+        this.date=date;
+        this.time=time;
+    }
+    
+    public void ShowPlaylist(){
+        System.out.println("Title "+title);
+        System.out.println("Album "+album);
+        System.out.println("Date "+date);
+        System.out.println("Time "+time);
+    }
+}
---
+++ src/main/java/Playlist.java
@@ -0,0 +1,11 @@
+/**
+ *
+ * @author DELL
+ */
+public class Playlist {
+
+    public static void main(String[] args) {
+        Musicas Pl1=new Musicas("El Paso Del Gigante","Cumbias Mexicanas","1987",3);
+        Pl1.ShowPlaylist();
+    }
+}
